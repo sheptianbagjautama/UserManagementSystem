d5178d0f7c79b21c16fd33eb51411ace42ed7833
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-05-22 21:44:16 +0700

    DBContext and migration

---
+++ UserManagementSystem.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementSystem.API.Models;
+
+namespace UserManagementSystem.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
