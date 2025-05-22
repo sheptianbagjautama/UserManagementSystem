709899e2c00a6bbeac70476c12452815c287590d
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-05-22 21:43:23 +0700

    dto

---
+++ UserManagementSystem.API/Dtos/RequestUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagementSystem.API.Dtos
+{
+    public class RequestUserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ UserManagementSystem.API/Dtos/ResponseUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagementSystem.API.Dtos
+{
+    public class ResponseUserDto
+    {
+        public required int Id { get; set; }
+        public required string Username { get; set; }
+        public required string FullName { get; set; }
+    }
+}
